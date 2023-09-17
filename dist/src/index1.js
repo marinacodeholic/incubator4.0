d775892150104025aa7ec15e390fb9f102db4f82
marina.codeholic <marina.codeholic@gmail.com>
2023-09-17 15:50:43 +0900

    changed port

--- dist/src/index1.js
+++ dist/src/index1.js
@@ -1,7 +1,7 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const app_1 = require("./app");
-const port = process.env.Port || 8089;
+const port = process.env.Port || 2211;
 app_1.app.listen(port, () => {
     console.log(`Example app listening on port ${port}`);
 });
--- src/index1.ts
+++ src/index1.ts
@@ -1,6 +1,6 @@
 import {app} from "./app";
 
-const port = process.env.Port || 8089
+const port = process.env.Port || 2211
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
